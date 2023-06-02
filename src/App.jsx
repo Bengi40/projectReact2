a45c10309c59e3c2e69ed1b05cf8c6caf6addcde
ELOISE DUFAU <eloise.dufau@gmail.com>
2023-06-02 19:55:20 +0200

    missing CSS

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const FFcharacters = [
     age:17,
     weapon:"sword",
     description :"Tidus se voit transporté 1000 ans dans le futur dans le monde de Spira, suite à l'attaque d'une entité nommée Sin. Il y rencontre Yuna, une jeune Invokeur dont il devient l'un des gardiens.",
-    Img:'https://upload.wikimedia.org/wikibooks/en/e/eb/Tidus_FFX.png'
+    Img:'https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-tidus-2.jpg'
   },
 
   {
@@ -21,7 +21,7 @@ const FFcharacters = [
     age:17,
     weapon:"scepter",
     description :"una décide de devenir Invokeur et d'accomplir son pèlerinage afin de détruire Sin. Au cours de son voyage, elle rencontre Tidus, et tous deux finiront par détruire Sin au prix d'un lourd sacrifice.",
-    Img:"https://static.wikia.nocookie.net/finalfantasy/images/0/0a/Yuna-FFX.jpg"
+    Img:"https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-yuna-2.jpg"
   },
 
   {
@@ -29,7 +29,7 @@ const FFcharacters = [
     age:17,
     weapon:"ball",
     description :"Capitaine et entraineur de l'équipe de blitzball des Besaid Aurochs, il abandonne son équipe après le tournoi de Luca pour devenir le gardien de Yuna et la suivre dans son pèlerinage.",
-    Img:"https://static.wikia.nocookie.net/finalfantasy/images/2/20/Wakka_Art.jpg"
+    Img:"https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-wakka-2.jpg"
   },
 
   {
@@ -37,39 +37,39 @@ const FFcharacters = [
     age:15,
     weapon:"daggers",
     description :"Fille du chef des Al Bheds et cousine de Yuna, elle devient la gardienne de celle-ci lors de son pèlerinage.",
-    Img:"https://static.wikia.nocookie.net/finalfantasy/images/7/71/Rikku_Art.jpg"
+    Img:"https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-rikku-2.jpg"
   },
 
   {
     name:"Auron",
     age:35,
     weapon:"sword",
-    information :"Auron est un homme qui fait preuve d'un sang-froid irréprochable durant la totalité de l'histoire, il se montre peu bavard et implacable sur le plan du Pèlerinage de Yuna, il ne veut en rien l'arrêter et est déterminé à vaincre Sin aux côtés de l'Invokeur et de ses Gardiens.",
-    Img:"https://static.wikia.nocookie.net/finalfantasy/images/f/f3/Auron_Art.png"
+    description :"Auron est un homme qui fait preuve d'un sang-froid irréprochable durant la totalité de l'histoire, il se montre peu bavard et implacable sur le plan du Pèlerinage de Yuna, il ne veut en rien l'arrêter et est déterminé à vaincre Sin aux côtés de l'Invokeur et de ses Gardiens.",
+    Img:"https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-auron-2.jpg"
   },
 
   {
     name:"Lulu",
     age:22,
     weapon:"Magic",
-    informations:"Lulu est un personnage jouable dans Final Fantasy X et un personnage non-jouable dans Final Fantasy X-2. Anciennement fiancée à Chappu, le frère de Wakka, jusqu'à ce qu'il meure, elle est désormais la gardienne de Yuna qu'elle considère comme sa sœur",
-    Img:"https://finalfantasy.fandom.com/fr/wiki/Lulu?file=Lulu_Art.jpg"
+    description:"Lulu est un personnage jouable dans Final Fantasy X et un personnage non-jouable dans Final Fantasy X-2. Anciennement fiancée à Chappu, le frère de Wakka, jusqu'à ce qu'il meure, elle est désormais la gardienne de Yuna qu'elle considère comme sa sœur",
+    Img:"https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-lulu-2.jpg"
   },
 
   {
     name:"Kimahri",
     age:25,
     weapon:"Spear",
-    informations:"Membre de la tribu des Ronsos, il est taciturne, à cause de la honte d'avoir eu sa corne brisée par un autre Ronso. Protecteur de Yuna depuis son enfance, il devient un Gardien pendant son pèlerinage.",
-    Img:"https://static.wikia.nocookie.net/finalfantasy/images/0/00/Kimahri_Ronso_artwork.jpg/"
+    description:"Membre de la tribu des Ronsos, il est taciturne, à cause de la honte d'avoir eu sa corne brisée par un autre Ronso. Protecteur de Yuna depuis son enfance, il devient un Gardien pendant son pèlerinage.",
+    Img:"https://www.supersoluce.com/sites/default/files/logo1/000-image-illu-final-fantasy-10-kimahri-ronso-2.jpg"
   },
 
   {
     name:"Seymour",
     age:28,
     weapon:"scepter",
-    informations:"Seymour Guado est un antagoniste dans le jeu Final Fantasy X. Il est temporairement jouable durant un combat contre Ghi pendant l'opération Mi'ihen. Il est le fils de lord Jyscal, un Guado.",
-    Img:"https://static.wikia.nocookie.net/finalfantasy/images/0/0f/Seymour_Guado_artwork.jpg"
+    description:"Seymour Guado est un antagoniste dans le jeu Final Fantasy X. Il est temporairement jouable durant un combat contre Ghi pendant l'opération Mi'ihen. Il est le fils de lord Jyscal, un Guado.",
+    Img:"https://www.pngkit.com/png/detail/64-645551_ffx-seymour-art-seymour-final-fantasy.png"
   },
 
 ]
@@ -77,7 +77,7 @@ const FFcharacters = [
 function App() {
  
   return (
-    <>
+    <>  
       <InputFirst/>
       <PresentationBox FFcharacters={FFcharacters}/>
     </>
--- src/components/PresentationBox.jsx
+++ src/components/PresentationBox.jsx
@@ -1,9 +1,10 @@
+import "./PresentationBox.css"
 
 const PresentationBox = ({FFcharacters}) => {
     return (
         <>
 {FFcharacters.map((character,index)=> (
-    <div key={index}>
+    <div className="box" key={index}>
     <h1>{character.name}</h1>
     <img src={character.Img} alt={character.name} />
     <p>{character.description}</p>
